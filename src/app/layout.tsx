0120146ed9041bf9ce3994ac86f11bd661f13ad0
Z User <z@container>
2026-06-19 15:28:14 +0000

    099dacdf-5a0b-4995-a1a8-ac3ad288f338

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,12 +40,7 @@ export const metadata: Metadata = {
   keywords: ["ENADE", "UEMS", "DIGES", "quiz", "votação", "tempo real"],
   authors: [{ name: "UEMS/DIGES" }],
   icons: {
-    icon: [
-      { url: "/favicon.ico", sizes: "any" },
-      { url: "/favicon.png", type: "image/png", sizes: "64x64" },
-    ],
-    shortcut: "/favicon.ico",
-    apple: "/favicon.png",
+    icon: "/logo.png",
   },
 };
 
